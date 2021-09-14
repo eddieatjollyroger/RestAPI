8a0d74a5d80fd4353652d924219a6fa5c8884e1f
eddieatjollyroger <38026492+eddieatjollyroger@users.noreply.github.com>
2021-09-14 02:07:11 +0000

    posting property

--- backend/routes/propertyRouter.js
+++ backend/routes/propertyRouter.js
@@ -1,17 +1,27 @@
 import express from 'express';
 
-
+import { readFile } from 'fs/promises';
+const json = JSON.parse(
+  await readFile(
+    new URL('../properties.json', import.meta.url)
+  )
+);
 
 const router = express.Router();
 
 
 router.get("/", (req, res) => {
-     console.log(JSON.stringify(properties))
-     res.send(JSON.stringify(properties));
+     console.log(json)
+     res.send(json);
 })
 
 router.post("/", (req, res) => {
-    console.log(req.body);
+    console.log(json.properties);
+    var properties = [];
+    properties.push(json.properties);
+    properties.push(req.body);
+    json.properties = properties;
+    res.send(json);
 })
 
 export default router;
